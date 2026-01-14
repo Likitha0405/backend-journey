84c6709e5e6d85f80b687894b05b544e2f6d79e7
Likitha_v <likithav62@gmail.com>
2026-01-14 20:09:27 +0530

    added basic python file

--- day1_basics.py
+++ day1_basics.py
@@ -1,1 +1,9 @@
+#Day 1 - lets learn about printing things on screen
+print("Hello my world!")
+print("This is my first step in backend development")
 
+#variables
+name = "likitha"
+role = "backend-software-developer"
+print("Name:", name)
+print("Role:", role)
